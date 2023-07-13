776fa9456a2617ca69bd7dd92ca74d61e51dfa79
EvgeniyHalimon <halimonevgeniy@gmail.com>
2023-07-13 19:40:57 +0300

    specie-1: setup project

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Daily budget app" />
+</svelte:head>
+
+<section />
---
+++ server/src/server.ts
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import express, { Application } from 'express';
+import morgan from 'morgan';
+
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use(express.static('public'));
+
+app.get('/ping', async (_req, res) => {
+  res.send({
+    message: 'pong!',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running on port', PORT);
+});
