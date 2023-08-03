619cf7d61f0abc19d0bc8ea00ebf64ece83b96e8
EvgeniyHalimon <halimonevgeniy@gmail.com>
2023-08-03 12:44:11 +0300

    specie-6: added emoji utils

--- client/src/components/TableModalContent.svelte
+++ client/src/components/TableModalContent.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
 	import { getDate } from '$shared/date';
 	import type { IBill } from '$shared/types';
-	import { getName } from '$shared/utils';
+	import { getCategoryEmoji, getName, getSubcategoryEmoji } from '$shared/utils';
 	import { categories, subcategories } from '$store/store';
 	export let bill: IBill;
-	console.log('🚀 ~ file: TableModalContent.svelte:7 ~ bill:', bill);
 </script>
 
 <div class="flex flex-col gap-2">
-	<h2>{getName(bill.categoryID, $categories)}</h2>
-	<h3>{getName(bill.subcategoryID, $subcategories)}</h3>
+	<h2>{getName(bill.categoryID, $categories)} {getCategoryEmoji(bill.categoryID)}</h2>
+	<h3>{getName(bill.subcategoryID, $subcategories)} {getSubcategoryEmoji(bill.subcategoryID)}</h3>
 	<p>{bill.price} {'₴'}</p>
 	<p>Comment: {bill.comment}</p>
 	<p>When: {getDate(bill.createdAt)}</p>
--- client/src/shared/utils.ts
+++ client/src/shared/utils.ts
@@ -1,6 +1,28 @@
+import { categoriesEmoji, subcategoriesEmoji } from './staticData';
 import type { ICategory, ISubcategory } from './types';
 
-export const getName = (id: number, data: ISubcategory[] | ICategory[]) => {
+interface IEmoji {
+	id: number;
+	emoji: string;
+}
+
+const getName = (id: number, data: ISubcategory[] | ICategory[]) => {
 	const category = data.filter((c) => c.id == id);
 	return category[0].name;
 };
+
+const filterEmojiArray = (emojiArray: IEmoji[], id: number) => {
+	return emojiArray.filter((category: IEmoji) => category.id == id);
+};
+
+const getCategoryEmoji = (id: number): string => {
+	const emoji = filterEmojiArray(categoriesEmoji, id);
+	return emoji[0].emoji;
+};
+
+const getSubcategoryEmoji = (id: number): string => {
+	const emoji = filterEmojiArray(subcategoriesEmoji, id);
+	return emoji[0].emoji;
+};
+
+export { getName, getCategoryEmoji, getSubcategoryEmoji };
